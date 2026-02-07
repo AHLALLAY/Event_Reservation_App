7d0b77828c0956215d7430cd68c4f8daa0559ab0
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-07 23:21:05 +0100

    KAN-31 Créer events.entity.ts

---
+++ backend/src/modules/events/entity/events.entity.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export enum EventStatus {
+    draft = 'draft',
+    published = 'published',
+    canceled = 'canceled',
+}
+
+@Entity('events')
+export class Event {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 20 })
+    title: string;
+
+    @Column({ type: 'text', nullable: true})
+    description: string;
+
+    @Column({ type: 'timestamptz' })
+    startDate: Date;
+
+    @Column({ type: 'timestamptz' })
+    endDate: Date;
+
+    @Column({ length: 100 })
+    place: string;
+
+    @Column({ type: 'int', default: 1 })
+    capacity: number;
+
+    @Column({ type: 'enum', enum: EventStatus, default: EventStatus.draft })
+    status: EventStatus;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
