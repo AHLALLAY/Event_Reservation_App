48878aff44c3eb5c87ee50f141f7781618c4dd7c
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-08 00:58:14 +0100

    KAN-34 Implémenter createEvent()

--- backend/src/modules/events/events.controller.ts
+++ backend/src/modules/events/events.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { EventService } from "./events.service";
+import { EventDto } from "./dtos/events.dto";
 
 @Controller()
 export class EventController {
     constructor(private eventService: EventService) { }
-    
+
+    @Post()
+    async createEvent(eventData: EventDto) {
+        return this.eventService.createEvent(eventData);
+    }
 }
--- backend/src/modules/events/events.service.ts
+++ backend/src/modules/events/events.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository} from "typeorm";
+import { Repository } from "typeorm";
 import { Event } from "./entity/events.entity";
+import { EventDto } from "./dtos/events.dto";
 
 
 @Injectable()
@@ -11,5 +12,12 @@ export class EventService {
         private eventRepo: Repository<Event>
     ) { }
 
-    
+    async createEvent(event: EventDto) {
+        const evnt = this.eventRepo.create({
+            ...event,
+            startDate: new Date(event.startDate),
+            endDate: new Date(event.endDate),
+        });
+        return this.eventRepo.save(evnt);
+    }
 }
