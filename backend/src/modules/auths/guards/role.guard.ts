5a36015f628698467c971ce8ae6ba84c091d6a98
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-07 22:37:43 +0100

    KAN-24 Créer role.guard.ts

---
+++ backend/src/modules/auths/guards/role.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { UserRole } from "src/shared/user.role";
+
+export const ROLES_KEY = 'roles';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRole = this.reflector.get<UserRole[]>(ROLES_KEY, context.getHandler());
+        if (!requiredRole?.length) return true;
+        const { user } = context.switchToHttp().getRequest();
+        if (!user?.role) throw new ForbiddenException('Accés Refusé');
+        if (!requiredRole.includes(user.role)) throw new ForbiddenException('Role inexiste')
+        return true;
+    }
+}
