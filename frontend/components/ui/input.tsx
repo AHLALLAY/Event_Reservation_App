abbc4f9d714db5d60a50c1ede5c73c294888fe62
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-09 14:52:47 +0100

    style: Ajouter un style par default pour 'Input'

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -6,12 +6,13 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 }
 
 export function Input({ label, error, id, type = "text", className, onChange, value, ...restProps }: InputProps) {
+    const defaultStyle = `border rounded px-3 py-2 ${className}`
     return (
-        <div className="space-y-2">
+        <div className="flex flex-col space-y-2">
             {label && (
                 <label htmlFor={id}>{label}</label>
             )}
-            <input type={type} id={id} className={className} onChange={onChange} value={value} {...restProps} />
+            <input type={type} id={id} className={defaultStyle} onChange={onChange} value={value} {...restProps} />
             {error && <span className="text-sm text-red-500 dark:text-red-400">{error}</span>}
         </div>
     );
