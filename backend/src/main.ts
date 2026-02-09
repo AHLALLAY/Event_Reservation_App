5c03c90d25a86244527240846e54848a40e99e02
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-09 14:28:27 +0100

    config: Configurer CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
