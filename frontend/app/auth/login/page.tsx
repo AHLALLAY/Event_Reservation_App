2c2227b026c55a55204ab88a035b95d958898ed3
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-09 15:49:18 +0100

    feat: [KAN-71] Créer la page /auth/login/page.tsx

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+export default function Login() {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError('');
+        setLoading(true);
+        try {
+            if (!email || !password) {
+                setError("All fields are required");
+                return;
+            }
+            const res = await fetch('http://localhost:3001/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            })
+            const data = await res.json();
+            if (!res.ok) {
+                setError(data.message || "login failed");
+                return;
+            }
+            localStorage.setItem('token', data.token);
+            router.push(`/${data.user.role}/`);
+
+        } catch (err) {
+            setError(err instanceof Error ? err.message : "");
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <div className="max-w-sm mx-auto p-6">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 border rounded-lg p-2">
+                <div className="flex justify-center">
+                    <h1 className="text-xl font-semibold">Login</h1>
+                </div>
+                {error && <span className="text-red-500 text-sm">{error}</span>}
+                <div className="flex flex-col space-y-4">
+                    <Input label="email" id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="" />
+                    <Input label="password" id="password" value={password} type="password" onChange={(e) => setPassword(e.target.value)} className="" minLength={8} />
+                </div>
+                <Button type="submit" disabled={loading}>{"login"}</Button>
+
+                <div className="felx text-center text-sm font-meduim">
+                    <u><a href="/auth/register">register</a></u>
+                </div>
+            </form>
+        </div>
+    );
+}
