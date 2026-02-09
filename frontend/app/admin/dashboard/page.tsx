3b796a72c6ae75228fd78e03a648fe8b401d0bca
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-02-09 21:47:02 +0100

    feat: Utiliser 'header.tsx' dans '/admin/dashboard'

--- frontend/app/admin/dashboard/page.tsx
+++ frontend/app/admin/dashboard/page.tsx
@@ -1,32 +1,14 @@
 'use client';
 
 import { StatsCard } from "@/components/ui/statusCard";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
 import AdminLayout from "../layout";
+import NavBar from "@/components/ui/header";
 
 export default function AdminDashboard() {
-    const router = useRouter();
-
-    function logout() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        router.push('/auth/login');
-    }
 
     return (
         <AdminLayout>
-            <div className="m-8 p-2 border-b-2 flex justify-between">
-                <div>
-                    <Link href="/admin/dashboard" className="hover:border-b-1 rounded-b-lg p-2">Dashboard</Link>
-                    <Link href="/admin/events" className="ml-4 hover:border-b-1 rounded-b-lg p-2">Events</Link>
-                    <Link href="/admin/reservation" className="ml-4 hover:border-b-1 rounded-b-lg p-2">Booking</Link>
-                </div>
-                <div>
-                    <Button type="button" onClick={logout} className="bg-red-500 text-white hover:bg-red-600">Logout</Button>
-                </div>
-            </div>
+            <NavBar/>
             <div className="flex p-2">
                 <StatsCard
                     title="Événements par statut"
